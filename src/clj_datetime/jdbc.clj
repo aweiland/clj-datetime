3ba7e9a4cdc4e7702059683f37c60d0397fe1eec
aweiland <aweiland@gmail.com>
2016-01-01 17:44:55 -0500

    JDBC

--- src/clj_datetime/jdbc.clj
+++ src/clj_datetime/jdbc.clj
@@ -3,14 +3,14 @@
 
   To use in your project, just require the namespace:
 
-    => (require 'clj-time.jdbc)
+    => (require 'clj-datetime.jdbc)
     nil
 
   Doing so will extend the protocols defined by clojure.java.jdbc, which will
   cause java.sql.Timestamp objects in JDBC result sets to be coerced to
-  org.joda.time.DateTime objects, and vice versa where java.sql.Timestamp
+  java.time.ZonedDateTime objects, and vice versa where java.sql.Timestamp
   objects would be required by JDBC."
-  (:require [clj-time.coerce :as tc]
+  (:require [clj-datetime.coerce :as tc]
             [clojure.java.jdbc :as jdbc]))
 
 ; http://clojure.github.io/java.jdbc/#clojure.java.jdbc/IResultSetReadColumn
@@ -23,10 +23,10 @@
     (tc/from-sql-date v))
   java.sql.Time
   (result-set-read-column [v _2 _3]
-    (org.joda.time.DateTime. v)))
+    (tc/from-date v)))
 
 ; http://clojure.github.io/java.jdbc/#clojure.java.jdbc/ISQLValue
 (extend-protocol jdbc/ISQLValue
-  org.joda.time.DateTime
+  java.time.ZonedDateTimeDateTime
   (sql-value [v]
     (tc/to-sql-time v)))
